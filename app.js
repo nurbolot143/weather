d582e75f6cd338f93c3546b0eaf5ee60f7601a29
Nurbolot Boobekov <nurbolot.boobekov.pls@gmail.com>
2022-03-31 18:41:16 +0300

    Changed index.html to index.ejs, add style.css, add weather https

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const https = require("https");
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.post("/", (req, res) => {
+  const query = req.body.cityName;
+  const apiKey = "3d1d6b9619dbbc96836b1983191a406b";
+  const unit = "metric";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${apiKey}&units=${unit}`;
+  https.get(url, function (response) {
+    console.log(response.statusCode);
+
+    if (response.statusCode == 200) {
+      response.on("data", function (data) {
+        const weatherData = JSON.parse(data);
+        const temp = Math.round(weatherData.main.temp);
+        const weatherDescription = weatherData.weather[0].description;
+        const city = weatherData.name;
+        const icon = weatherData.weather[0].icon;
+        const imageURL = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+        res.render("index", {
+          temp: temp,
+          cityName: city,
+          weatherDescription: weatherDescription,
+        });
+      });
+    } else {
+      res.render("index");
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log("Server is started on port", port);
+});
